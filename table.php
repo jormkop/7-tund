0e48b0e970dcde05cee854565220e0a440cec2cb
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-10-14 10:22:02 +0300

    added files

---
+++ table.php
@@ -0,0 +1,68 @@
+<?php
+	require_once("functions.php");
+	
+	//kas kustutame
+	//?delete=vastav id mida kustutada on aadressireal
+	
+	if(isset($_GET["delete"])){
+		echo "kustutame id ".$_GET["delete"];
+		//käivitame munktsiooni, saadan id kaasa
+		deleteCar($_GET["delete"]);
+	}
+	//salvestab ab'i
+	if(isset($_POST["save"])){
+		
+		updateCar($_POST["$id"], $_POST["plate_number"], $_POST["color"]);
+	}
+	//käivitan funktsiooni
+	$array_of_cars = getCarData();
+	
+	//trükin välja esimese auto
+	//echo $array_of_cars[0]->id." ".$array_of_cars[0]->plate;
+?>
+
+<h2>Tabel</h2>
+<table border=1>
+	<tr>
+		<th>id</th>
+		<th>numbrimärk</th>
+		<th>värv</th>
+		<th>user_id</th>
+		<th>kustuta</th>
+		<th>Edit</th>
+	</tr>
+	
+	<?php
+		//trükime välja read
+		//massiivi pikkus count()
+		for($i = 0; $i < count($array_of_cars); $i++){
+			//echo $array_of_cars[$i]->id;
+			
+			if(isset($_GET["edit"]) && $array_of_cars[$i]->id == $_GET["edit"]){
+			
+			echo "<tr>";
+			echo "<form action= 'table.php' method='post'>";
+			echo "<input type='hidden' name='id' value='".$array_of_cars[$i]->id."'>";
+			echo "<td>".$array_of_cars[$i]->id."</td>";
+			echo "<td><input name='plate_number' value='".$array_of_cars[$i]->plate."'></td>";
+			echo "<td><input name='color' value='".$array_of_cars[$i]->color."'></td>";
+			echo "<td>".$array_of_cars[$i]->user_id."</td>";
+			echo "<td><a href='table.php'>cancel<a></td>";
+			echo "<td><input type='submit' name='save'></td>";
+			echo "</form>";
+			echo "</tr>";
+				
+			}else{			
+			echo "<tr>";
+			echo "<td>".$array_of_cars[$i]->id."</td>";
+			echo "<td>".$array_of_cars[$i]->plate."</td>";
+			echo "<td>".$array_of_cars[$i]->color."</td>";
+			echo "<td>".$array_of_cars[$i]->user_id."</td>";
+			echo "<td><a href='?delete=".$array_of_cars[$i]->id."'>X</a></td>";
+			echo "<td><a href='?edit=".$array_of_cars[$i]->id."'>edit</a></td>";
+			echo "</tr>";
+		}
+	}
+
+	?>
+</table>
